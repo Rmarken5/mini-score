e49c3563a01ac454947f5d382b02a78f2c14d6f2
Ryan Marken <marken.ryan@gmail.com>
2023-07-03 11:13:44 -0400

    Shuffle some code, add index, update go version in docker file

---
+++ service/internal/general/http/handlers/home-handler.go
@@ -0,0 +1,32 @@
+package handlers
+
+import (
+	"embed"
+	"github.com/labstack/echo/v4"
+	"html/template"
+	"log"
+)
+
+//go:embed templates/index.gotmpl
+var files embed.FS
+
+type (
+	IndexHandler struct {
+		logger *log.Logger
+	}
+)
+
+func NewIndexHandler(logger *log.Logger) *IndexHandler {
+	return &IndexHandler{logger: logger}
+}
+
+func (h *IndexHandler) ServeHTTP(c echo.Context) error {
+
+	file, err := template.ParseFS(files, "templates/index.gotmpl")
+	if err != nil {
+		h.logger.Printf("Error opening file from template: %w", err)
+		return err
+	}
+
+	return file.Execute(c.Response(), nil)
+}
